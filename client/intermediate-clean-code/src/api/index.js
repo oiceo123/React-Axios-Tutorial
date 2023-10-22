b4c55a123e4ba909a79607ded37990a5b7e7d100
oiceo123 <iceome44@gmail.com>
2023-10-23 01:47:46 +0700

    ✨ สร้าง axios instance

---
+++ client/intermediate-clean-code/src/api/index.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+// Create Instance
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 5000, // 5000ms => 5s
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    // ทำการเซ็ต config บางอย่างก่อนที่จะส่ง request เช่น เซ็ต token ก่อนส่ง request
+    // get api key
+    const apiKey = process.env.REACT_APP_API_KEY;
+    if (apiKey) {
+      config.headers["X-Api-Key"] = apiKey;
+    }
+    // get access token
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // เมื่อการส่ง request มีข้อผิดพลาดจะเข้ามาทำงานตรงนี้
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    // หากได้รับ status code ที่อยู่ในช่วง 2xx จะเข้ามาทำงานตรงนี้
+    return response;
+  },
+  (error) => {
+    // หากได้รับ error จาก response จะเข้ามาทำงานตรงนี้
+    if (error.response && error.response.status === 401) {
+      console.log("Unauthorize");
+      return;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
